e001d393416822f3f446c0f2be3fb8223bde6569
ossot0223 <168603641+ossot0223@users.noreply.github.com>
2024-07-04 08:52:15 -0500

    app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+function App() {
+
+
+    const estilo = {
+      backgroundColor: 'red',
+      color: 'white',
+      padding: '10px',
+    }
+  
+    return (
+      <div className="App">
+        <h1 style={estilo}>Hola mundo</h1>
+      </div>
+    );
+  }
+  
+  export default App;
