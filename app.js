f1c51097a0714012c7ac16de83aa033b4d9e05e7
ossot0223 <168603641+ossot0223@users.noreply.github.com>
2024-07-04 09:23:41 -0500

    app.js

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
+export default App;
 
-    const estilo = {
-      backgroundColor: 'red',
-      color: 'white',
-      padding: '10px',
-    }
-  
-    return (
-      <div className="App">
-        <h1 style={estilo}>Hola mundo</h1>
-      </div>
-    );
-  }
-  
-  export default App;
